57bd49962538c70d1d6f4e3a651e2b134c2b7d24
artagnancoder <stayhomer@outlook.com>
2021-10-28 14:23:54 -0300

    Criação dos arquivos iniciais e instalação das bibliotecas.

---
+++ qrcode-generator/src/App.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@200&display=swap');
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Inconsolata', monospace;
+    color: white;
+
+  }
+  body{
+    background-color: #121212;
+  }
+`
+const App = () => {
+  return (
+    <div>
+      <GlobalStyle />
+
+      LET'S GO
+    </div>
+  );
+}
+
+export default App;
